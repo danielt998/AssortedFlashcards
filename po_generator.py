825b7b055f56c4f21fd4aa70de0ed1334b280a5a
Daniel Thomas <danielt998@gmail.com>
2018-12-12 21:10:09 +0000

    add todo

--- po_generator.py
+++ po_generator.py
@@ -1,4 +1,8 @@
 #Read in a po file and create flashcards based on the source and target strings
+#TODO:
+#some sort of more 'fuzzy' duplicate detection - some strings are very similar - for now just using sort|uniq...
+#remove numbers before duplicate detection?
+#ignore anything with no Chinese characters
 import polib
 import sys
 po = polib.pofile(sys.argv[1])
